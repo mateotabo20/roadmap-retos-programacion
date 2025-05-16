17995679c7f324171b0d8e1c313e929d1ea8e614
mateotabo20 <mateotaboada2001@gmail.com>
2025-05-16 19:16:09 -0400

    mateotabo20.cpp
    .

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c++/mateotabo20.cpp
@@ -0,0 +1,27 @@
+//https://isocpp.org
+
+/*esto tambien es 
+un comentario en 
+varias lineas
+*/
+
+#include <iostream>// biblioteca
+
+using namespace std;
+
+const int edad = 19;
+
+int main(){
+int numero=20;
+float decimal=34,5;
+string nommbre="Mateo";
+char elemento= 'm':
+bool boleano= false
+
+unsigned int positivo= 455; //solo posistivo
+long long int largoj = 2342353464564; //cantidad larga
+
+  string lenguaje = "c++"
+
+  <<cout "hola ,"<<lenguaje<<"!"<<endl;
+}
